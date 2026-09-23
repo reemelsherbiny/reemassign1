968e539be7e61f3c6c0b8996d9fb32a6abe8ce32
Reem Elsherbiny <reem.elsherbiny@student.giu-uni.de>
2026-09-23 18:39:26 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+const heroName = "reem";
+const studentId = 16009072;
+const power = studentId * 9;
+const warcry = "By the Sinai dunes, the Ash Phoenix flees!";
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log(warcry);
